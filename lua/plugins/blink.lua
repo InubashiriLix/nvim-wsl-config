aa03101720f5f519828817cb2d16054bf7c970a1
InubashiriLix <lixinrong4002@gmail.com>
2026-02-27 18:37:59 +0800

    feat(greek): update use_greek_completion option in blink cmp cfg file

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,3 +1,5 @@
+local use_greek_completion = true
+
 return {
     "saghen/blink.cmp",
     -- optional: provides snippets for the snippet source
@@ -12,50 +14,44 @@ return {
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
-    opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = "super-tab" },
 
-        appearance = {
+    opts = function(_, opts)
+        opts.keymap = { preset = "super-tab" }
+
+        opts.appearance = {
             -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
             -- Adjusts spacing to ensure icons are aligned
             nerd_font_variant = "mono",
-        },
+        }
 
         -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = false } },
+        opts.completion = { documentation = { auto_show = false } }
 
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
-        sources = {
-            default = { "lsp", "path", "snippets", "buffer", "greek" },
-            providers = {
-                greek = {
-                    name = "Greek",
-                    module = "completion.greek",
-                    score_offset = -2,
-                    min_keyword_length = 1,
-                },
-            },
-        },
+        opts.sources = {
+            -- default = { "lsp", "path", "snippets", "buffer", "greek" },
+            default = { "lsp", "path", "snippets", "buffer" },
+            -- providers = {
+            -- greek = {
+            --     name = "Greek",
+            --     module = "completion.greek",
+            --     score_offset = -2,
+            --     min_keyword_length = 1,
+            -- },
+            -- },
+        }
+
+        if use_greek_completion then
+            opts.sources.default = vim.list_extend(opts.sources.default, { "greek" })
+            opts.sources.providers =
+                { greek = { name = "Greek", module = "completion.greek", score_offset = -2, min_keyword_length = 1 } }
+        end
 
         -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
         -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
         -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
         --
         -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "prefer_rust_with_warning" },
-    },
+        opts.fuzzy = { implementation = "prefer_rust_with_warning" }
+    end,
     opts_extend = { "sources.default" },
 }
