a96cbf0d3648c5d1af3ad2420da7ed9ea8b2809b
InubashiriLix <lixinrong4002@gmail.com>
2026-03-09 17:33:53 +0800

    chore(swap warning): load newest swap if current buffer has no change. - note: go check autocmds to findout detailed imple logic

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+local group = vim.api.nvim_create_augroup("auto_reload_external_changes", { clear = true })
+
+vim.api.nvim_create_autocmd("FileChangedShell", {
+    group = group,
+    callback = function(args)
+        if vim.bo[args.buf].modified then
+            vim.notify(
+                "检测到外部修改，但当前 buffer 也有未保存改动，未自动覆盖",
+                vim.log.levels.WARN
+            )
+            return
+        end
+        vim.cmd("silent! checktime")
+    end,
+})
